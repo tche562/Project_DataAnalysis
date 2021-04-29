8ce66b53e08b8b18920458525e638a1e230f407c
tche562@aucklanduni.ac.nz <48459355+tche562@users.noreply.github.com>
2021-04-29 22:17:06 +1200

    use case.docx updated_1

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,9 +1,19 @@
-import pandas
+import pandas as pd
 import numpy
 import csv
 
-filename = "melb_data.csv"
+pd.set_option('display.max_rows', 20)
+data = pd.read_csv('melb_data.csv')
+#print(data.ioc[1])#row
+#print(data['Suburb'])#colume
+
+print(data)
 
 
 
+#data.to_csv('./test.csv',sep=',',header = True, index = True)
 
+#print(data['Date'])#colume
+
+for item in data['Date']:
+    print(item)
