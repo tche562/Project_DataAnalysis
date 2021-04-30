b6c155e13ff2d0a290223fd4c8a6d9c52b3d79df
tche562@aucklanduni.ac.nz <48459355+tche562@users.noreply.github.com>
2021-04-30 18:43:19 +1200

    use case.docx preprocesing_1

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,13 +1,15 @@
 import pandas as pd
-import numpy
+import numpy as np
 import csv
+import datetime
+import time
 
-pd.set_option('display.max_rows', 20)
+pd.set_option('display.max_rows', 50)
 data = pd.read_csv('melb_data.csv')
 #print(data.ioc[1])#row
 #print(data['Suburb'])#colume
 
-print(data)
+
 
 
 
@@ -15,5 +17,21 @@ print(data)
 
 #print(data['Date'])#colume
 
-for item in data['Date']:
-    print(item);
+data['Date'] = pd.to_datetime(data['Date'],format='%d/%m/%Y')
+data['Year'] = data['Date'].dt.year
+data['Month'] = data['Date'].dt.month
+# for item in data['Date']:
+#     print(item)
+#     date_time = datetime.datetime.strptime(item, '%d/%m/%Y')
+#     print(date_time)
+#     data['Year'] = date_time
+
+new = np.unique(data['Year'])
+print(new)
+
+a = data.isnull().sum().sort_values(ascending = False)
+# a = a.sort_value(axis=1,ascending = False)
+print(a)
+# print(data)
+
+#data.to_csv('./test.csv',sep=',',header = True, index = True)
